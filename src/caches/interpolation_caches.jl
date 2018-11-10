70090fe4cfa0dd7721c74f50b0266c8d664a6237
shubham maddhashiya <shubhamsipah@iitkgp.ac.in>
2018-11-10 13:59:01 +0530

    p as optional

--- src/caches/interpolation_caches.jl
+++ src/caches/interpolation_caches.jl
@@ -189,14 +189,14 @@ function Curvefit(u,t,m,p)
 end
 
 # SigmoidFit
-function SigmoidFit(u,t,p)
+function SigmoidFit(u,t,p=zeros(2))
   m = (x, p) -> @. p[1]/(1+exp(x-p[2]))
   c_f = curve_fit(m,t,u,p)
   Curvefit{true}(u,t,m,c_f)
 end
 
 # HillFit
-function HillFit(u,t,p,n=nothing)
+function HillFit(u,t,p=zeros(2),n=nothing)
   if n == nothing
     push!(p,rand(1)[1])
     m = (x, p) -> @. p[1] * inv(1.0 + p[2]/(x^p[3]))
@@ -208,7 +208,7 @@ function HillFit(u,t,p,n=nothing)
 end
 
 # WeibullFit
-function WeibullFit(u,t,p)
+function WeibullFit(u,t,p=zeros(3))
   m = weibull_fun
   c_f = curve_fit(m,t,u,p)
   Curvefit{true}(u,t,m,c_f)
