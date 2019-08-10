97bfa36e362dca62c604e9ca63f25e85f8b61691
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-08-10 10:37:08 +0530

    First pass at series recipes

--- src/plot_rec.jl
+++ src/plot_rec.jl
@@ -1,3 +1,7 @@
+################################################################################
+#                                 Type recipes                                 #
+################################################################################
+
 @recipe function f(A::AbstractInterpolation; plotdensity = 10_000, denseplot = true)
   t = sort(A.t)
   start = t[1]; stop = t[end]
@@ -23,3 +27,80 @@ end
   end
   plott, A(plott)
 end
+
+################################################################################
+#                                Series recipes                                #
+################################################################################
+
+
+############################################################
+#                      Interpolations                      #
+############################################################
+
+########################################
+#         Linear Interpolation         #
+########################################
+
+@recipe function f(::Type{Val{:linear_interp}}, x, y, z)
+
+    label --> "Linear fit"
+
+    LinearInterpolation(y, x)
+end
+
+########################################
+#       Quadratic Interpolation        #
+########################################
+
+@recipe function f(::Type{Val{:quadratic_interp}}, x, y, z) # TODO are these good values?
+
+    label --> "Quadratic fit"
+
+    QuadraticInterpolation(y, x)
+end
+
+########################################
+#           Quadratic Spline           #
+########################################
+
+@recipe function f(::Type{Val{:quadratic_spline}}, x, y, z)
+
+    label --> "Quadratic Spline"
+
+    QuadraticSpline(y, x)
+end
+
+########################################
+#        Lagrange Interpolation        #
+########################################
+
+@recipe function f(::Type{Val{:lagrange_interp}}, x, y, z; n = length(x) - 1)
+
+    label --> "Lagrange Fit"
+
+    LagrangeInterpolation(y, x, n)
+end
+
+########################################
+#             Cubic Spline             #
+########################################
+
+@recipe function f(::Type{Val{:cubic_spline}}, x, y, z)
+
+    label --> "Cubic Spline"
+
+    CubicSpline(y, x)
+end
+
+########################################
+#                Loess                 #
+########################################
+
+@recipe function f(::Type{Val{:loess}}, x, y, z; d = 5, α = 0.75) # TODO are these good values?
+
+    label --> "LOESS fit"
+
+    Loess(y, x, d, α)
+end
+
+# @recipe function f(::Type{Val{:bspline_interp}}, x, y; pVec=:)
