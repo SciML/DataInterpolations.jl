35a9e6bfc3d305b20752e25fb0b8b138c357527f
avik-pal <avikpal@iitk.ac.in>
2020-08-06 16:12:06 -0400

    Tests passing for methods other than BSpline

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using DataInterpolations, Test
 
 @testset "Interface" begin include("interface.jl") end
 @testset "Interpolation Tests" begin include("interpolation_tests.jl") end
+@testset "Derivative Tests" begin include("derivative_tests.jl") end
