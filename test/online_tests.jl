7828c97d363676304168e9e251186cf2f5b0e1b0
Pepijn de Vos <pepijndevos@gmail.com>
2021-12-22 12:34:24 +0100

    underp and break tests

--- test/online_tests.jl
+++ test/online_tests.jl
@@ -4,7 +4,7 @@ t = [1,2,3]
 u = [0, 1, 0]
 
 for di in [LinearInterpolation, QuadraticInterpolation, ConstantInterpolation]
-    li = LinearInterpolation(copy(u), copy(t))
+    li = di(copy(u), copy(t))
     append!(li, u, t)
-    @test li == LinearInterpolation(vcat(u, u), vcat(t, t))
+    @test li == di(vcat(u, u), vcat(t, t))
 end
