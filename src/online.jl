91879102d502a9736499f71bc2c6ad370b6fdfe6
Pepijn de Vos <pepijndevos@gmail.com>
2021-12-22 13:30:44 +0100

    add push impl

--- src/online.jl
+++ src/online.jl
@@ -1,4 +1,22 @@
-import Base: append!
+import Base: append!, push!
+
+function push!(di::LinearInterpolation{U, T}, u::eltype(U), t::eltype(T)) where {U, T}
+  push!(di.u, u)
+  push!(di.t, t)
+  di
+end
+
+function push!(di::QuadraticInterpolation{U, T}, u::eltype(U), t::eltype(T)) where {U, T}
+  push!(di.u, u)
+  push!(di.t, t)
+  di
+end
+
+function push!(di::ConstantInterpolation{U, T}, u::eltype(U), t::eltype(T)) where {U, T}
+  push!(di.u, u)
+  push!(di.t, t)
+  di
+end
 
 function append!(di::LinearInterpolation{U, T}, u::U, t::T) where {U, T}
   u, t = munge_data(u, t)
--- test/online_tests.jl
+++ test/online_tests.jl
@@ -7,4 +7,8 @@ for di in [LinearInterpolation, QuadraticInterpolation, ConstantInterpolation]
     li = di(copy(u), copy(t))
     append!(li, u, t)
     @test li == di(vcat(u, u), vcat(t, t))
+
+    li = di(copy(u), copy(t))
+    push!(li, 4, 1)
+    @test li == di(vcat(u, 1), vcat(t, 4))
 end
