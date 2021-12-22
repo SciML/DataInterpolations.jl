384f28dcac95d8754a1edb18c0b04cde8a48cc1f
Pepijn de Vos <pepijndevos@gmail.com>
2021-12-22 14:07:11 +0100

    underp tests once more

--- test/online_tests.jl
+++ test/online_tests.jl
@@ -9,6 +9,6 @@ for di in [LinearInterpolation, QuadraticInterpolation, ConstantInterpolation]
     @test li == di(vcat(u, u), vcat(t, t))
 
     li = di(copy(u), copy(t))
-    push!(li, 4, 1)
+    push!(li, 1, 4)
     @test li == di(vcat(u, 1), vcat(t, 4))
 end
