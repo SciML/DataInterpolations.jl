10bb033a4f98cc62ebeaacf6a42e4d002c0bddcd
Andreas Noack <andreas@noack.dk>
2022-11-18 14:27:29 +0100

    Revert "Merge pull request #104 from jjstickel/regularization"
    This reverts commit 894bfce56e785aefbfefdc35f3f89ccadb21397a, reversing
    changes made to c5acb4e56ed8c9e929d4ae56622d16d4fb831458.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,3 @@ using DataInterpolations, Test
 @testset "Derivative Tests" begin include("derivative_tests.jl") end
 @testset "Integral Tests" begin include("integral_tests.jl") end
 @testset "Online Tests" begin include("online_tests.jl") end
-@testset "Regularization Smoothing" begin include("regularization.jl") end
