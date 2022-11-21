8f2eaf8aebb47830f792cb1715867a2f075fb4a1
Andreas Noack <andreas@noack.dk>
2022-11-21 08:56:01 +0100

    Revert "Merge pull request #99 from jjstickel/regularization" (#128)
    * Revert "Merge pull request #99 from jjstickel/regularization"
    
    This reverts commit 2f56d6039a235b1bd2ca74332191c2d51d793442, reversing
    changes made to 4b6fad18b42651f7b94c1455f83fe2696f4da016.
    
    * Revert "Merge pull request #104 from jjstickel/regularization"
    
    This reverts commit 894bfce56e785aefbfefdc35f3f89ccadb21397a, reversing
    changes made to c5acb4e56ed8c9e929d4ae56622d16d4fb831458.
    
    * Update README.md
    
    Co-authored-by: JuliusMartensen <julius.martensen@gmail.com>
    
    Co-authored-by: JuliusMartensen <julius.martensen@gmail.com>

--- src/DataInterpolations.jl
+++ src/DataInterpolations.jl
@@ -7,11 +7,10 @@ abstract type AbstractInterpolation{FT,T} <: AbstractVector{T} end
 Base.size(A::AbstractInterpolation) = size(A.u)
 Base.size(A::AbstractInterpolation{true}) = length(A.u) .+ size(A.t)
 Base.getindex(A::AbstractInterpolation,i) = A.u[i]
-Base.getindex(A::AbstractInterpolation{true},i) =
-    i<=length(A.u) ? A.u[i] : A.t[i-length(A.u)]
+Base.getindex(A::AbstractInterpolation{true},i) = i<=length(A.u) ? A.u[i] : A.t[i-length(A.u)]
 Base.setindex!(A::AbstractInterpolation,x,i) = A.u[i] = x
 Base.setindex!(A::AbstractInterpolation{true},x,i) =
-    i <= length(A.u) ? (A.u[i] = x) : (A.t[i-length(A.u)] = x)
+                                   i <= length(A.u) ? (A.u[i] = x) : (A.t[i-length(A.u)] = x)
 
 using ChainRulesCore, LinearAlgebra, RecursiveArrayTools, RecipesBase, Reexport
 @reexport using Optim
@@ -24,15 +23,13 @@ include("derivatives.jl")
 include("integrals.jl")
 include("online.jl")
 
-function ChainRulesCore.rrule(::typeof(_interpolate),
-                              A::Union{LagrangeInterpolation,AkimaInterpolation,
-                                       BSplineInterpolation,BSplineApprox}, t::Number)
+function ChainRulesCore.rrule(::typeof(_interpolate), A::Union{LagrangeInterpolation,AkimaInterpolation,
+                                                               BSplineInterpolation,BSplineApprox}, t::Number)
     interpolate_pullback(Δ) = (NoTangent(), NoTangent(), derivative(A, t) * Δ)
     return _interpolate(A, t), interpolate_pullback
 end
 
-ChainRulesCore.frule((_, _, Δt), ::typeof(_interpolate), A::AbstractInterpolation,
-                     t::Number) = _interpolate(A, t), derivative(A, t) * Δt
+ChainRulesCore.frule((_, _, Δt), ::typeof(_interpolate), A::AbstractInterpolation, t::Number) = _interpolate(A, t), derivative(A, t) * Δt
 
 (interp::AbstractInterpolation)(t::Number) = _interpolate(interp, t)
 
@@ -40,15 +37,9 @@ using Symbolics: Num, unwrap, SymbolicUtils
 (interp::AbstractInterpolation)(t::Num) = SymbolicUtils.term(interp, unwrap(t))
 SymbolicUtils.promote_symtype(t::AbstractInterpolation, _...) = Real
 
-export LinearInterpolation, QuadraticInterpolation, LagrangeInterpolation,
-    AkimaInterpolation, ConstantInterpolation, QuadraticSpline, CubicSpline,
-    BSplineInterpolation, BSplineApprox, Curvefit
 
-# added for RegularizationSmooth, JJS 11/27/21
-import RegularizationTools
-const RT = RegularizationTools
-include("regularization_smooth.jl")
-export RegularizationSmooth
+export LinearInterpolation, QuadraticInterpolation, LagrangeInterpolation, AkimaInterpolation,
+       ConstantInterpolation, QuadraticSpline, CubicSpline, BSplineInterpolation, BSplineApprox, Curvefit
 
 # Deprecated April 2020
 export ZeroSpline
