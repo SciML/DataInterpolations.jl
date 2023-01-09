1ae3e8287793fd57b41e52d68e8eda6049041e0f
Glen Hertz <glen.hertz@gmail.com>
2023-01-09 19:00:29 +0000

    Make Symbolics a conditional dependency in 1.9
    Also tested on 1.8 and is backwards compatible.

---
+++ ext/SymbolicsExt.jl
@@ -0,0 +1,8 @@
+module SymbolicsExt
+
+using DataInterpolations: AbstractInterpolation
+using Symbolics: Num, unwrap, SymbolicUtils
+(interp::AbstractInterpolation)(t::Num) = SymbolicUtils.term(interp, unwrap(t))
+SymbolicUtils.promote_symtype(t::AbstractInterpolation, _...) = Real
+
+end # module
