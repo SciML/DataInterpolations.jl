6ff7725f313856a67ad3925e4bee8a6fd3d80ef1
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-02-24 07:58:27 +0100

    Implement basic `Base.nameof` for interpolations
    ModelingToolkit throws errors when trying to print systems with interpolations due to calling `Base.nameof` on expression atoms.

--- ext/DataInterpolationsSymbolicsExt.jl
+++ ext/DataInterpolationsSymbolicsExt.jl
@@ -4,5 +4,6 @@ using DataInterpolations: AbstractInterpolation
 using Symbolics: Num, unwrap, SymbolicUtils
 (interp::AbstractInterpolation)(t::Num) = SymbolicUtils.term(interp, unwrap(t))
 SymbolicUtils.promote_symtype(t::AbstractInterpolation, _...) = Real
+Base.nameof(interp::AbstractInterpolation) = :Interpolation
 
 end # module
