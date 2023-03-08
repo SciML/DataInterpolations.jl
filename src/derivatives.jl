c705b1ab54aea2f93a7b11a6fb91804884b3321d
user.email <michael.oliver.boyle@gmail.com>
2023-03-07 23:04:22 -0500

    Track last index in interpolations

--- src/derivatives.jl
+++ src/derivatives.jl
@@ -1,33 +1,36 @@
-function derivative(A::LinearInterpolation{<:AbstractVector}, t::Number)
-  idx = searchsortedfirst(A.t, t)
+derivative(A, t) = derivative(A, t, firstindex(A.t)-1)[1]
+
+
+function derivative(A::LinearInterpolation{<:AbstractVector}, t::Number, iguess)
+  idx = searchsortedfirst(A.t, t, iguess)
   if A.t[idx] >= t
     idx -= 1
   end
   idx == 0 ? idx += 1 : nothing
   θ = 1 / (A.t[idx+1] - A.t[idx])
-  (A.u[idx+1] - A.u[idx]) / (A.t[idx+1] - A.t[idx])
+  (A.u[idx+1] - A.u[idx]) / (A.t[idx+1] - A.t[idx]), idx
 end
 
-function derivative(A::LinearInterpolation{<:AbstractMatrix}, t::Number)
-  idx = searchsortedfirst(A.t, t)
+function derivative(A::LinearInterpolation{<:AbstractMatrix}, t::Number, iguess)
+  idx = searchsortedfirst(A.t, t, iguess)
   if A.t[idx] >= t
     idx -= 1
   end
   idx == 0 ? idx += 1 : nothing
   θ = 1 / (A.t[idx+1] - A.t[idx])
-  @views @. (A.u[:, idx+1] - A.u[:, idx]) / (A.t[idx+1] - A.t[idx])
+  (@views @. (A.u[:, idx+1] - A.u[:, idx]) / (A.t[idx+1] - A.t[idx])), idx
 end
 
-function derivative(A::QuadraticInterpolation{<:AbstractVector}, t::Number)
-  i₀, i₁, i₂ = _quad_interp_indices(A, t)
+function derivative(A::QuadraticInterpolation{<:AbstractVector}, t::Number, iguess)
+  i₀, i₁, i₂ = _quad_interp_indices(A, t, iguess)
   dl₀ = (2t - A.t[i₁] - A.t[i₂]) / ((A.t[i₀] - A.t[i₁]) * (A.t[i₀] - A.t[i₂]))
   dl₁ = (2t - A.t[i₀] - A.t[i₂]) / ((A.t[i₁] - A.t[i₀]) * (A.t[i₁] - A.t[i₂]))
   dl₂ = (2t - A.t[i₀] - A.t[i₁]) / ((A.t[i₂] - A.t[i₀]) * (A.t[i₂] - A.t[i₁]))
-  A.u[i₀] * dl₀ + A.u[i₁] * dl₁ + A.u[i₂] * dl₂
+  A.u[i₀] * dl₀ + A.u[i₁] * dl₁ + A.u[i₂] * dl₂, i₀
 end
 
-function derivative(A::QuadraticInterpolation{<:AbstractMatrix}, t::Number)
-  idx = searchsortedfirst(A.t, t)
+function derivative(A::QuadraticInterpolation{<:AbstractMatrix}, t::Number, iguess)
+  idx = searchsortedfirst(A.t, t, iguess)
   if A.t[idx] >= t
       idx -= 1
   end
@@ -40,7 +43,7 @@ function derivative(A::QuadraticInterpolation{<:AbstractMatrix}, t::Number)
   dl₀ = (2t - A.t[i₁] - A.t[i₂]) / ((A.t[i₀] - A.t[i₁]) * (A.t[i₀] - A.t[i₂]))
   dl₁ = (2t - A.t[i₀] - A.t[i₂]) / ((A.t[i₁] - A.t[i₀]) * (A.t[i₁] - A.t[i₂]))
   dl₂ = (2t - A.t[i₀] - A.t[i₁]) / ((A.t[i₂] - A.t[i₀]) * (A.t[i₂] - A.t[i₁]))
-  @views @. A.u[:, i₀] * dl₀ + A.u[:, i₁] * dl₁ + A.u[:, i₂] * dl₂
+  (@views @. A.u[:, i₀] * dl₀ + A.u[:, i₁] * dl₁ + A.u[:, i₂] * dl₂), idx
 end
 
 function derivative(A::LagrangeInterpolation{<:AbstractVector}, t::Number)
@@ -109,13 +112,13 @@ function derivative(A::LagrangeInterpolation{<:AbstractMatrix}, t::Number)
   @. (DG * F - G * DF) / (F ^ 2)
 end
 
-function derivative(A::AkimaInterpolation{<:AbstractVector}, t::Number)
-  t < A.t[1] && return zero(A.u[1])
-  t > A.t[end] && return zero(A.u[end])
-  i = searchsortedlast(A.t, t)
+function derivative(A::AkimaInterpolation{<:AbstractVector}, t::Number, iguess)
+  t < A.t[1] && return zero(A.u[1]), 1
+  t > A.t[end] && return zero(A.u[end]), lastindex(t)
+  i = searchsortedlast(A.t, t, iguess)
   j = min(i, length(A.c))  # for smooth derivative at A.t[end]
   wj = t - A.t[i]
-  @evalpoly wj A.b[i] 2A.c[j] 3A.d[j]
+  (@evalpoly wj A.b[i] 2A.c[j] 3A.d[j]), i
 end
 
 function derivative(A::ConstantInterpolation{<:AbstractVector}, t::Number)
@@ -127,27 +130,27 @@ function derivative(A::ConstantInterpolation{<:AbstractMatrix}, t::Number)
 end
 
 # QuadraticSpline Interpolation
-function derivative(A::QuadraticSpline{<:AbstractVector}, t::Number)
-  i = searchsortedfirst(A.t, t)
+function derivative(A::QuadraticSpline{<:AbstractVector}, t::Number, iguess)
+  i = searchsortedfirst(A.t, t, iguess)
   i == 1 ? i += 1 : nothing
   σ = 1//2 * (A.z[i] - A.z[i - 1]) / (A.t[i] - A.t[i - 1])
-  A.z[i-1] + 2σ * (t - A.t[i-1])
+  A.z[i-1] + 2σ * (t - A.t[i-1]), i
 end
 
 # CubicSpline Interpolation
-function derivative(A::CubicSpline{<:AbstractVector}, t::Number)
-  i = searchsortedfirst(A.t, t)
+function derivative(A::CubicSpline{<:AbstractVector}, t::Number, iguess)
+  i = searchsortedfirst(A.t, t, iguess)
   isnothing(i) ? i = length(A.t) - 1 : i -= 1
   i == 0 ? i += 1 : nothing
   dI = -3A.z[i] * (A.t[i + 1] - t)^2 / (6A.h[i + 1]) + 3A.z[i + 1] * (t - A.t[i])^2 / (6A.h[i + 1])
   dC = A.u[i + 1] / A.h[i + 1] - A.z[i + 1] * A.h[i + 1] / 6
   dD = -(A.u[i] / A.h[i + 1] - A.z[i] * A.h[i + 1] / 6)
-  dI + dC + dD
+  dI + dC + dD, i
 end
 
-function derivative(A::BSplineInterpolation{<:AbstractVector{<:Number}}, t::Number)
+function derivative(A::BSplineInterpolation{<:AbstractVector{<:Number}}, t::Number, iguess)
   # change t into param [0 1]
-  idx = searchsortedlast(A.t,t)
+  idx = searchsortedlast(A.t,t, iguess)
   idx == length(A.t) ? idx -= 1 : nothing
   n = length(A.t)
   scale = (A.p[idx+1] - A.p[idx]) / (A.t[idx+1] - A.t[idx])
@@ -157,13 +160,13 @@ function derivative(A::BSplineInterpolation{<:AbstractVector{<:Number}}, t::Numb
   for i = 1:(n - 1)
     ducum += N[i + 1] * (A.c[i + 1] - A.c[i]) / (A.k[i + A.d + 1] - A.k[i + 1])
   end
-  ducum * A.d * scale
+  ducum * A.d * scale, idx
 end
 
 # BSpline Curve Approx
-function derivative(A::BSplineApprox{<:AbstractVector{<:Number}}, t::Number)
+function derivative(A::BSplineApprox{<:AbstractVector{<:Number}}, t::Number, iguess)
   # change t into param [0 1]
-  idx = searchsortedlast(A.t,t)
+  idx = searchsortedlast(A.t,t, iguess)
   idx == 0 ? idx += 1 : nothing
   scale = (A.p[idx+1] - A.p[idx]) / (A.t[idx+1] - A.t[idx])
   t_ = A.p[idx] + (t - A.t[idx]) * scale
@@ -172,5 +175,5 @@ function derivative(A::BSplineApprox{<:AbstractVector{<:Number}}, t::Number)
   for i = 1:(A.h - 1)
     ducum += N[i + 1] * (A.c[i + 1] - A.c[i]) / (A.k[i + A.d + 1] - A.k[i + 1])
   end
-  ducum * A.d * scale
+  ducum * A.d * scale, idx
 end
