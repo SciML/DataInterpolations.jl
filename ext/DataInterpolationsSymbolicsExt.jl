665b490e53bb50e03c648c6c96ba5b96ea231d33
Sathvik Bhagavan <sathvik.bhagavan@juliahub.com>
2024-05-06 07:55:43 +0000

    refactor: add dispatch for higher order derivatives with Symbolics

--- ext/DataInterpolationsSymbolicsExt.jl
+++ ext/DataInterpolationsSymbolicsExt.jl
@@ -21,6 +21,10 @@ function derivative(interp::AbstractInterpolation, t::Num, order = 1)
 end
 SymbolicUtils.promote_symtype(::typeof(derivative), _...) = Real
 
+function Symbolics.derivative(::typeof(derivative), args::NTuple{3, Any}, ::Val{2})
+    Symbolics.unwrap(derivative(args[1], Symbolics.wrap(args[2]), args[3] + 1))
+end
+
 function Symbolics.derivative(interp::AbstractInterpolation, args::NTuple{1, Any}, ::Val{1})
     Symbolics.unwrap(derivative(interp, Symbolics.wrap(args[1])))
 end
