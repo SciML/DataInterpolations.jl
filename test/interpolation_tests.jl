e71c5521805db2e40aacd2be4b17651b22dd15b1
Ashutosh Bharambe <a.bharambe123@gmail.com>
2024-10-26 08:42:45 +0000

    test(BSplines): add tests

--- test/interpolation_tests.jl
+++ test/interpolation_tests.jl
@@ -664,7 +664,20 @@ end
         @test_throws DataInterpolations.ExtrapolationError A(-1.0)
         @test_throws DataInterpolations.ExtrapolationError A(300.0)
 
-        @testset "Vector{Vector}" begin end
+        @testset "Vector{Vector}" begin
+            t = 0.1:0.1:1.0
+            u_vec = [[sin.(t_), cos.(t_)] for t_ in t]
+            A = BSplineInterpolation(u_vec, t, 2, :Uniform, :Uniform)
+            t_test = 0.1:0.05:1.0
+            u_test = reduce(hcat, A.(t_test))
+            @test isapprox(u_test[1, :], sin.(t_test), atol = 1e-3)
+            @test isapprox(u_test[2, :], cos.(t_test), atol = 1e-3)
+
+            A = BSplineInterpolation(u_vec, t, 2, :ArcLen, :Average)
+            u_test = reduce(hcat, A.(t_test))
+            @test isapprox(u_test[1, :], sin.(t_test), atol = 1e-2)
+            @test isapprox(u_test[2, :], cos.(t_test), atol = 1e-2)
+        end
 
         @testset "AbstractMatrix" begin
             t = 0.1:0.1:1.0
