083878cbca61c064962fd9659328c95575818e1e
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-07-26 22:27:10 -0400

    Semi ok Makie extension (first pass)
    This has an issue where calling a specific recipe will work fine,
    but calling `plot` will not utilize cycled color.

---
+++ ext/DataInterpolationsMakieExt.jl
@@ -0,0 +1,41 @@
+module DataInterpolationsMakieExt
+
+using DataInterpolations
+using DataInterpolations: AbstractInterpolation
+using Makie
+
+# Define the default type of plot that you want
+Makie.plottype(::AbstractInterpolation) = Makie.ScatterLines
+
+# Define the attributes that you want to use
+Makie.used_attributes(::Makie.PointBased, ::AbstractInterpolation) = (:plotdensity, :denseplot)
+Makie.used_attributes(::Type{<:Makie.ScatterLines}, ::AbstractInterpolation) = (:plotdensity, :denseplot)
+
+# Define the conversion of the data to the plot
+function Makie.convert_arguments(
+    ::Makie.PointBased,
+    A::AbstractInterpolation;
+    plotdensity = 10_000,
+    denseplot = true,
+)
+    DataInterpolations.to_plottable(A; plotdensity = plotdensity, denseplot = denseplot)
+end
+
+# Define the conversion of the data to the plot for the ScatterLines type
+# Note that this is a bit of a hack
+# and should actually be handled by a plot! method,
+# except that doesn't work anymore or does it?
+function Makie.convert_arguments(
+    ::Type{<:Makie.ScatterLines},
+    A::AbstractInterpolation;
+    plotdensity = 10_000,
+    denseplot = true,
+)
+    densex, densey = convert_arguments(Makie.PointBased(), A; plotdensity = plotdensity, denseplot = denseplot)
+    return [
+        Makie.SpecApi.Lines(densex, densey),
+        Makie.SpecApi.Scatter(A.t, A.u)
+    ]
+end
+
+end # module
