a967cdfefbf6902e6f02b2556ea2ac3ee29386ae
John Biffl <86376592+jbiffl@users.noreply.github.com>
2026-07-24 16:01:52 -0400

    Matrix u extrapolation tests added
    Added tests for the following matrix u extrapolation methods:
    -ConstantInterpolation w/ extrapolation type Constant
    -LinearInterpolation w/ extrapolation types Constant, Linear, Extension
    -QuadraticInterpolation w/ extrapolation types Constant, Linear, Extension

--- test/extrapolation_tests.jl
+++ test/extrapolation_tests.jl
@@ -97,12 +97,58 @@ end
     @test @inferred(A([t_eval])) == [0.0u"m"]
     @test A([t_eval]) isa Vector{typeof(0.0u"m")}
 
-    # Right constant extrapolation
+    # Right linear extrapolation
     A = LinearInterpolation(u_un, t_un; extrapolation_right = ExtrapolationType.Linear)
     t_eval = 3.0u"s"
     @test @inferred(A(t_eval)) == 3.0u"m"
     @test @inferred(A([t_eval])) == [3.0u"m"]
     @test A([t_eval]) isa Vector{typeof(3.0u"m")}
+end
+
+@testset "Constant Interpolation" begin
+    u = [1.0, 2.0]
+    t = [1.0, 2.0]
+
+    #test_extrapolation(ConstantInterpolation, u, t)
+
+    extrapolation_type = ExtrapolationType.Constant
+    # Left extrapolation
+    A = ConstantInterpolation(u, t; extrapolation_left = extrapolation_type)
+    t_eval = 0.0
+    @test A(t_eval) == 1.0
+    @test DataInterpolations.derivative(A, t_eval) == 0.0
+    @test DataInterpolations.derivative(A, t_eval, 2) == 0.0
+    @test DataInterpolations.integral(A, t_eval) == -1.0
+    t_eval = 3.0
+
+    # Right extrapolation
+    A = ConstantInterpolation(u, t; extrapolation_right = extrapolation_type)
+    t_eval = 3.0
+    @test A(t_eval) == 2.0
+    @test DataInterpolations.derivative(A, t_eval) == 0.0
+    @test DataInterpolations.derivative(A, t_eval, 2) == 0.0
+    @test DataInterpolations.integral(A, t_eval) == 3.0
+
+    #Matrix u tests
+    u = [1.0 2.0; 1.0 2.0]
+    t = [1.0, 2.0]
+    extrapolation_type = ExtrapolationType.Constant
+    # Left extrapolation
+    A = ConstantInterpolation(u, t; extrapolation_left = extrapolation_type)
+    t_eval = 0.0
+    @test A(t_eval) == [1.0, 1.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) == [-1.0, -1.0]
+
+    # Right extrapolation
+    A = ConstantInterpolation(u, t; extrapolation_right = extrapolation_type)
+    t_eval = 3.0
+    @test A(t_eval) == [2.0, 2.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) == [3.0, 3.0]
+        
 end
 
 @testset "Linear Interpolation" begin
@@ -130,6 +176,45 @@ end
         @test DataInterpolations.integral(A, t_eval) == 4.0
         t_eval = 0.0
     end
+
+    #Matrix u tests
+    u = [1.0 2.0; 2.0 5.0]
+    t = [1.0, 2.0]
+    extrapolation_type = ExtrapolationType.Constant
+    # Left extrapolation
+    A = LinearInterpolation(u, t; extrapolation_left = extrapolation_type)
+    t_eval = 0.0
+    @test A(t_eval) == [1.0, 2.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) == [-1.0, -2.0]
+
+    # Right extrapolation
+    A = LinearInterpolation(u, t; extrapolation_right = extrapolation_type)
+    t_eval = 3.0
+    @test A(t_eval) == [2.0, 5.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) == [3.5, 8.5]
+    
+    for extrapolation_type in [ExtrapolationType.Linear, ExtrapolationType.Extension]
+        # Left extrapolation
+        A = LinearInterpolation(u, t; extrapolation_left = extrapolation_type)
+        t_eval = 0.0
+        @test A(t_eval) == [0.0, -1.0]
+        @test DataInterpolations.derivative(A, t_eval) == [1.0, 3.0]
+        @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+        @test DataInterpolations.integral(A, t_eval) == [-0.5, -0.5]
+        
+        # Right extrapolation
+        A = LinearInterpolation(u, t; extrapolation_right = extrapolation_type)
+        t_eval = 3.0
+        @test A(t_eval) == [3.0, 8.0]
+        @test DataInterpolations.derivative(A, t_eval) == [1.0, 3.0]
+        @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+        @test DataInterpolations.integral(A, t_eval) == [4.0, 10.0]
+    end
+        
 end
 
 @testset "Quadratic Interpolation" begin
@@ -171,4 +256,60 @@ end
     @test DataInterpolations.derivative(A, t_eval) == df(t_eval)
     @test DataInterpolations.derivative(A, t_eval, 2) == -3
     @test DataInterpolations.integral(A, t_eval) ≈ 5.25
+
+    #Matrix u tests
+    u = [1.0 3.0 2.0; 1.0 3.0 2.0]
+    t = 1:3
+
+    test_extrapolation(QuadraticInterpolation, u, t)
+
+    # Constant left extrapolation
+    A = QuadraticInterpolation(u, t; extrapolation_left = ExtrapolationType.Constant)
+    t_eval = 0.0
+    @test A(t_eval) ≈ [1.0, 1.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) ≈ [-1.0, -1.0]
+
+    # Constant right extrapolation
+    A = QuadraticInterpolation(u, t; extrapolation_right = ExtrapolationType.Constant)
+    t_eval = 4.0
+    @test A(t_eval) ≈ [2.0, 2.0]
+    @test DataInterpolations.derivative(A, t_eval) == [0.0, 0.0]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t[end], t_eval) ≈ [2.0, 2.0]
+
+    # Linear left extrapolation
+    A = QuadraticInterpolation(u, t; extrapolation_left = ExtrapolationType.Linear)
+    t_eval = 0.0
+    @test A(t_eval) ≈ [-2.5,-2.5]
+    @test DataInterpolations.derivative(A, t_eval) == [3.5, 3.5]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t_eval) ≈ [0.75, 0.75]
+
+    # Linear right extrapolation
+    A = QuadraticInterpolation(u, t; extrapolation_right = ExtrapolationType.Linear)
+    t_eval = 4.0
+    @test A(t_eval) ≈ [-0.5, -0.5]
+    @test DataInterpolations.derivative(A, t_eval) == [-2.5, -2.5]
+    @test DataInterpolations.derivative(A, t_eval, 2) == [0.0, 0.0]
+    @test DataInterpolations.integral(A, t[end], t_eval) ≈ [0.75, 0.75]
+
+    # Extension left extrapolation
+    f = t -> (-3t^2 + 13t - 8) / 2
+    df = t -> (-6t + 13) / 2
+    A = QuadraticInterpolation(u, t; extrapolation_left = ExtrapolationType.Extension)
+    t_eval = 0.0
+    @test A(t_eval) ≈ [-4.0, -4.0]
+    @test DataInterpolations.derivative(A, t_eval) == df(t_eval) * ones(2)
+    @test DataInterpolations.derivative(A, t_eval, 2) == [-3, -3]
+    @test DataInterpolations.integral(A, t_eval) ≈ [1.25, 1.25]
+
+    # Extension right extrapolation
+    A = QuadraticInterpolation(u, t; extrapolation_right = ExtrapolationType.Extension)
+    t_eval = 4.0
+    @test A(t_eval) ≈ [-2.0, -2.0]
+    @test DataInterpolations.derivative(A, t_eval) == df(t_eval) * ones(2)
+    @test DataInterpolations.derivative(A, t_eval, 2) == [-3, -3]
+    @test DataInterpolations.integral(A, t_eval) ≈ [5.25, 5.25]
 end
